9775b7cb4299ec565321bfa3e998a6cbf58a59ad
alexa <alexandru.matei.sporis@gmail.com>
2025-02-20 20:51:18 +0200

    Added camera functionality and started with input conditions

---
+++ app/src/main/java/com/example/application/CameraActivity.kt
@@ -0,0 +1,175 @@
+package com.example.application
+
+import android.Manifest
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class CameraActivity : AppCompatActivity() {
+
+    private lateinit var cameraExecutor: ExecutorService
+    private var imageCapture: ImageCapture? = null
+    private lateinit var previewView: PreviewView
+    private lateinit var captureButton: Button
+    private var capturedImageUri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_camera)
+
+        previewView = findViewById(R.id.previewView)
+        val startCameraButton = findViewById<Button>(R.id.startCameraButton)
+        captureButton = findViewById(R.id.captureButton)
+
+        startCameraButton.setOnClickListener {
+            onButtonClicked()
+        }
+
+        captureButton.setOnClickListener {
+            takePhoto()
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        val backButton = findViewById<Button>(R.id.backButton)
+        backButton.setOnClickListener {
+            val resultIntent = Intent()
+            resultIntent.putExtra("capturedImageUri", capturedImageUri.toString()) // Send Image URI
+            setResult(RESULT_OK, resultIntent)
+            finish() // Close CameraActivity
+        }
+    }
+
+    private fun onButtonClicked() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this, Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                startCamera()
+            }
+
+            shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
+                // Explain to the user why the permission is needed...
+                requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+            }
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            startCamera()
+        } else {
+            Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder().build().also {
+                it.surfaceProvider = previewView.surfaceProvider
+            }
+
+            imageCapture = ImageCapture.Builder().build()
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create a timestamped name and MediaStore entry.
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(
+            this.contentResolver,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues
+        ).build()
+
+        // Set up image capture listener, which is triggered after photo has been taken
+        imageCapture.takePicture(outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    capturedImageUri = output.savedUri
+                    val msg = "Photo captured: $capturedImageUri"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d("CameraActivity", msg)
+
+                    displayCapturedImage(capturedImageUri)
+                }
+            })
+    }
+
+    private fun displayCapturedImage(imageUri: Uri?) {
+        val capturedImageView = findViewById<ImageView>(R.id.imgCaptured)
+
+        // Load the captured image URI into the ImageView
+        imageUri?.let {
+            capturedImageView?.setImageURI(it)
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    }
+}
--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.application
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import org.json.JSONObject
 import java.io.File
@@ -17,12 +19,19 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private val fileName = "user_data.json"
+    private lateinit var imageCameraKennzeichen: ImageView
+    private lateinit var imageCameraIdent: ImageView
+    private lateinit var imageCameraBrief: ImageView
+    private lateinit var imageCameraVorne: ImageView
+    private lateinit var imageCameraHinten: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val inputKennzeichen = findViewById<EditText>(R.id.inputKennzeichen)
+        val inputKennzeichen1 = findViewById<EditText>(R.id.inputKennzeichen1)
+        val inputKennzeichen2 = findViewById<EditText>(R.id.inputKennzeichen2)
+        val inputKennzeichen3 = findViewById<EditText>(R.id.inputKennzeichen3)
         val inputIdent = findViewById<EditText>(R.id.inputIdent)
         val inputBrief = findViewById<EditText>(R.id.inputBrief)
         val inputVorne = findViewById<EditText>(R.id.inputVorne)
@@ -34,17 +43,27 @@ class MainActivity : AppCompatActivity() {
 
         // Save JSON data when the button is clicked
         saveButton.setOnClickListener {
-            val jsonObject = JSONObject()
-            jsonObject.put("Kennzeichen", inputKennzeichen.text)
-            jsonObject.put("Ident", inputIdent.text)
-            jsonObject.put("Brief", inputBrief.text)
-            jsonObject.put("Vorne", inputVorne.text)
-            jsonObject.put("Hinten", inputHinten.text)
+            if (inputKennzeichen.text.isEmpty() or inputIdent.text.isEmpty() or inputBrief.text.isEmpty() or inputVorne.text.isEmpty() or inputHinten.text.isEmpty()) {
+                Toast.makeText(this, "Field cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val jsonObject = JSONObject()
+                jsonObject.put("Kennzeichen", inputKennzeichen.text)
+                jsonObject.put("imageKennzeichen", imageCameraKennzeichen.toString())
+                jsonObject.put("Ident", inputIdent.text)
+                jsonObject.put("imageIdent", imageCameraIdent.toString())
+                jsonObject.put("Brief", inputBrief.text)
+                jsonObject.put("imageBrief", imageCameraBrief.toString())
+                jsonObject.put("Vorne", inputVorne.text)
+                jsonObject.put("imageVorne", imageCameraVorne.toString())
+                jsonObject.put("Hinten", inputHinten.text)
+                jsonObject.put("imageHinten", imageCameraHinten.toString())
 
-            if (writeJsonToFile(jsonObject)) {
-                textView.text = "JSON saved successfully!"
-            } else {
-                textView.text = "Failed to save JSON."
+                if (writeJsonToFile(jsonObject)) {
+                    textView.text = "JSON saved successfully!"
+                } else {
+                    textView.text = "Failed to save JSON."
+                }
             }
         }
 
@@ -134,6 +153,117 @@ class MainActivity : AppCompatActivity() {
         readButton.setOnClickListener {
             val jsonData = readJsonFromFile()
             textView.text = jsonData ?: "No JSON data found."
+        }
+
+        var cameraKennzeichen: Button
+        cameraKennzeichen = findViewById(R.id.cameraKennzeichen)
+        imageCameraKennzeichen = findViewById(R.id.imageCameraKennzeichen)
+        imageCameraKennzeichen.isVisible = false
+
+        cameraKennzeichen.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            cameraResultLauncherKennzeichen.launch(intent)
+        }
+
+        var cameraIdent: Button
+        cameraIdent = findViewById(R.id.cameraIdent)
+        imageCameraIdent = findViewById(R.id.imageCameraIdent)
+        imageCameraIdent.isVisible = false
+
+        cameraIdent.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            cameraResultLauncherIdent.launch(intent)
+        }
+
+        var cameraBrief: Button
+        cameraBrief = findViewById(R.id.cameraBrief)
+        imageCameraBrief = findViewById(R.id.imageCameraBrief)
+        imageCameraBrief.isVisible = false
+
+        cameraBrief.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            cameraResultLauncherBrief.launch(intent)
+        }
+
+        var cameraVorne: Button
+        cameraVorne = findViewById(R.id.cameraVorne)
+        imageCameraVorne = findViewById(R.id.imageCameraVorne)
+        imageCameraVorne.isVisible = false
+
+        cameraVorne.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            cameraResultLauncherVorne.launch(intent)
+        }
+
+        var cameraHinten: Button
+        cameraHinten = findViewById(R.id.cameraHinten)
+        imageCameraHinten = findViewById(R.id.imageCameraHinten)
+        imageCameraHinten.isVisible = false
+
+        cameraHinten.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            cameraResultLauncherHinten.launch(intent)
+        }
+    }
+
+    // Register for activity result
+    private val cameraResultLauncherKennzeichen = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val capturedImageUriString = result.data?.getStringExtra("capturedImageUri")
+            if (!capturedImageUriString.isNullOrEmpty()) {
+                val capturedImageUri = Uri.parse(capturedImageUriString)
+                imageCameraKennzeichen.setImageURI(capturedImageUri) // Show image in ImageView
+            }
+        }
+    }
+
+    private val cameraResultLauncherIdent = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val capturedImageUriString = result.data?.getStringExtra("capturedImageUri")
+            if (!capturedImageUriString.isNullOrEmpty()) {
+                val capturedImageUri = Uri.parse(capturedImageUriString)
+                imageCameraIdent.setImageURI(capturedImageUri) // Show image in ImageView
+            }
+        }
+    }
+
+    private val cameraResultLauncherBrief = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val capturedImageUriString = result.data?.getStringExtra("capturedImageUri")
+            if (!capturedImageUriString.isNullOrEmpty()) {
+                val capturedImageUri = Uri.parse(capturedImageUriString)
+                imageCameraBrief.setImageURI(capturedImageUri) // Show image in ImageView
+            }
+        }
+    }
+
+    private val cameraResultLauncherVorne = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val capturedImageUriString = result.data?.getStringExtra("capturedImageUri")
+            if (!capturedImageUriString.isNullOrEmpty()) {
+                val capturedImageUri = Uri.parse(capturedImageUriString)
+                imageCameraVorne.setImageURI(capturedImageUri) // Show image in ImageView
+            }
+        }
+    }
+
+    private val cameraResultLauncherHinten = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val capturedImageUriString = result.data?.getStringExtra("capturedImageUri")
+            if (!capturedImageUriString.isNullOrEmpty()) {
+                val capturedImageUri = Uri.parse(capturedImageUriString)
+                imageCameraHinten.setImageURI(capturedImageUri) // Show image in ImageView
+            }
         }
     }
 
